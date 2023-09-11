fdf1e5bce2fe814f20c182ee973917dd37826ed4
Rsyd <Rasyid2007AlKautsar@outlook.co.id>
2023-09-12 03:45:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -101,7 +101,6 @@ if [[ $arch = "arm64" ]]; then
         export CLANG_TRIPLE="aarch64-linux-gnu-"
         export CROSS_COMPILE="aarch64-linux-gnu-"
         export CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-    elif [[ $compiler = neutron-clang/* ]]; then
     elif [[ $compiler = azure-clang/* ]]; then
 
         ver="${compiler/azure-clang\/}"
